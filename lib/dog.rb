dca2e7cce0bd02e66069534369c61cc6fe6fb0ea
Mars Le <hp.mars.le@gmail.com>
2019-08-27 03:09:40 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,6 +3,17 @@ class Dog
   @@all = []
   
   
+  def initialize(name)
+    @name = name 
+    @@all << self 
+  end 
   
+  def self.all
+    @@all
+  end 
+  
+  def self.print_all
+    @@all.each{|puppy|puts puppy.name}
+  end 
   
 end 
